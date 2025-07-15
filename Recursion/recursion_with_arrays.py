ea254cb5180bd80e1b5630668e52cb19d516dd3f
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-07-15 07:08:58 +0530

    Completed the code checking if the array is sorted using recursion

--- Recursion/recursion_with_arrays.py
+++ Recursion/recursion_with_arrays.py
@@ -3,6 +3,7 @@
 arr = [1, 2, 3, 4, 5]
 a2rr = [1, 3, 2, 4, 5]  # Example of unsorted array
 
+print("Head recurstion")
 # head recursion
 def is_sorted(arr, index=0):
     n = len(arr)
@@ -16,6 +17,7 @@ def is_sorted(arr, index=0):
 print(is_sorted(arr))  # Output: True
 print(is_sorted(a2rr))  # Output: False
 
+print("Tail Recursion")
 # Tail recursion
 def is_sorted_tail(arr, index=0):
     n = len(arr)
@@ -28,3 +30,30 @@ def is_sorted_tail(arr, index=0):
 
 print(is_sorted_tail(arr))  # Output: True
 print(is_sorted_tail(a2rr))  # Output: False
+
+
+'''Alternate approach to check if array is sorted using recursion'''
+print("Alternate Approach head recursion")
+# head recursion
+def is_sorted_alternate(arr):
+    if len(arr) <= 1:
+        return True
+    ans = is_sorted_alternate(arr[1:])
+    if arr[0] <= arr[1]:
+        return ans
+    else:
+        return False
+
+print(is_sorted_alternate(arr))  # Output: True
+print(is_sorted_alternate(a2rr))  # Output: False
+
+print("Alternate Approach Tail recursion")
+def is_sorted_alternate_tail(arr):
+    if len(arr) <= 1:
+        return True
+    if arr[0] > arr[1]:
+        return False
+    return is_sorted_alternate_tail(arr[1:])
+
+print(is_sorted_alternate_tail(arr))  # Output: True
+print(is_sorted_alternate_tail(a2rr))  # Output: False
